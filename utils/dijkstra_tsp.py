d4a9b0f15eb37cfe3f469e309e968e63f9a4f04c
Marcos Paulo <marcos.silva4@arapiraca.ufal.br>
2024-03-17 22:53:24 -0300

    feat: final

--- utils/dijkstra_tsp.py
+++ utils/dijkstra_tsp.py
@@ -1,28 +1,49 @@
+class DijsktraTSP:
 
-def dijkstra_tsp(matrix, start):
-    num_cities = len(matrix)
-    visited = set([start])
-    path = [start]
-    current = start
-    while len(visited) < num_cities:
-        nearest_neighbor = None
-        nearest_distance = float('inf')
-        for neighbor in range(num_cities):
-            if neighbor not in visited:
-                distance = matrix[current][neighbor]
-                if distance < nearest_distance:
-                    nearest_neighbor = neighbor
-                    nearest_distance = distance
-        if nearest_neighbor is not None:
-            path.append(nearest_neighbor)
-            visited.add(nearest_neighbor)
-            current = nearest_neighbor
-        else:
-            break
-    path.append(start)  # Volta ao vértice inicial
-    return path
+    @staticmethod
+    def dijkstra_tsp(matrix, start):
+        num_cities = len(matrix)
+        visited = set([start])
+        path = [start]
+        current = start
+        while len(visited) < num_cities:
+            nearest_neighbor = None
+            nearest_distance = float('inf')
+            for neighbor in range(num_cities):
+                if neighbor not in visited:
+                    distance = matrix[current][neighbor]
+                    if distance < nearest_distance:
+                        nearest_neighbor = neighbor
+                        nearest_distance = distance
+            if nearest_neighbor is not None:
+                path.append(nearest_neighbor)
+                visited.add(nearest_neighbor)
+                current = nearest_neighbor
+            else:
+                break
+        path.append(start)  # Volta ao vértice inicial
+        return path
+    
+    @staticmethod
+    def bestDistance(matrizDistance):
+        num_cities = len(matrizDistance)
 
-# Exemplo de uso
+        countFinal=100000
+        tourFinal = []
+
+        for start_node in range(num_cities):
+            countAux=0
+            tour = DijsktraTSP.dijkstra_tsp(matrizDistance, start_node)
+            for i in range(len(tour)-1):
+                countAux+=matrizDistance[tour[i]][tour[i+1]]
+            if countAux<countFinal:
+                countFinal=countAux
+                tourFinal=tour
+
+        print("Caminho aproximado do PCV:", tourFinal)
+
+
+""" # Exemplo de uso
 distances = [
     [0, 10, 15, 20],
     [10, 0, 35, 25],
@@ -47,3 +68,4 @@ for start_node in range(num_cities):
 
 print("Caminho aproximado do PCV:", tourFinal)
 
+ """
